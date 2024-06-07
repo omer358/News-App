287709b196125d5e8d8bf9fa2d0b95c6ac84642c
omer358 <omermaki358@gmail.com>
2024-06-07 07:17:17 +0400

    impl DataStore

---
+++ app/src/main/java/com/example/newsapp/domain/manager/LocalUserManager.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.domain.manager
+
+import kotlinx.coroutines.flow.Flow
+
+interface LocalUserManager {
+    suspend fun saveAppEntry()
+    suspend fun readAppEntry(): Flow<Boolean>
+
+}
---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.newsapp.utils
+
+object Constants {
+    const val USER_SETTINGS = "userSettings"
+    const val APP_ENTRY = "appEntry"
+}
