cab339946e360d668ad9581badad94c47da22e0e
omer358 <omermaki358@gmail.com>
2024-06-07 11:56:02 +0400

    System UI Controller

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.example.newsapp.domain.usecases.AppEntryUseCases
 import com.example.newsapp.presentation.nvgraph.NavGraph
 import com.example.newsapp.ui.theme.NewsAppTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -28,6 +32,14 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             NewsAppTheme {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    systemUiController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
                 Surface {
                     val startDestination = viewModel.startDestination
                     NavGraph(startDestination = startDestination)
