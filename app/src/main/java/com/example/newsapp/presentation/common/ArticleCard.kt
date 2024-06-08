add0fb3067b75dde1d224c19fe7da851ac9e7d63
omer358 <omermaki358@gmail.com>
2024-06-09 02:41:04 +0400

    impl HomeScreen

--- app/src/main/java/com/example/newsapp/presentation/common/ArticleCard.kt
+++ app/src/main/java/com/example/newsapp/presentation/common/ArticleCard.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -51,6 +52,7 @@ fun ArticleCard(
                 .data(article.urlToImage)
                 .build(),
             contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(ArticleCarSize)
                 .clip(MaterialTheme.shapes.medium)
--- app/src/main/java/com/example/newsapp/presentation/nvgraph/NavGraph.kt
+++ app/src/main/java/com/example/newsapp/presentation/nvgraph/NavGraph.kt
@@ -1,28 +1,30 @@
 package com.example.newsapp.presentation.nvgraph
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.newsapp.presentation.home.HomeScreen
+import com.example.newsapp.presentation.home.HomeViewModel
 import com.example.newsapp.presentation.onboarding.OnBoardingScreen
 import com.example.newsapp.presentation.onboarding.OnBoardingViewModel
 
 @Composable
 fun NavGraph(
     startDestination: String,
-){
+) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = startDestination){
+    NavHost(navController = navController, startDestination = startDestination) {
         navigation(
             route = Routes.AppStartNavigation.route,
             startDestination = Routes.OnBoardingScreen.route
-        ){
+        ) {
             composable(
                 route = Routes.OnBoardingScreen.route
-            ){
+            ) {
                 val viewModel: OnBoardingViewModel = hiltViewModel()
                 OnBoardingScreen(
                     event = {
@@ -34,11 +36,14 @@ fun NavGraph(
         navigation(
             route = Routes.NewsNavigation.route,
             startDestination = Routes.NewsNavigatorScreen.route
-        ){
+        ) {
             composable(
                 route = Routes.NewsNavigatorScreen.route
-            ){
-                Text(text = "News Navigator Screen")
+            ) {
+                val viewModel: HomeViewModel = hiltViewModel()
+                val articles = viewModel.news.collectAsLazyPagingItems()
+                HomeScreen(articles = articles, navigate = {})
+
             }
         }
     }
