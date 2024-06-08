775c7e08098f0c5c37e61ff813256fdd909d359b
omer358 <omermaki358@gmail.com>
2024-06-08 16:35:02 +0400

    impl fetching news
    config di
    impl NewsUseCases

--- app/src/main/java/com/example/newsapp/domain/usecases/news/GetNews.kt
+++ app/src/main/java/com/example/newsapp/domain/usecases/news/GetNews.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.Flow
 class GetNews(
     private val newsRepository: NewsRepository
 ) {
-    suspend operator fun invoke(sources: List<String>): Flow<PagingData<Article>> {
+     operator fun invoke(sources: List<String>): Flow<PagingData<Article>> {
         return  newsRepository.getNews(sources = sources)
     }
 }
--- app/src/main/java/com/example/newsapp/utils/Constants.kt
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
     const val API_KEY = "70f5aae4e4a040129a561e0b0ca6a225"
+    const val BASE_URL = "https://newsapi.org/v2/"
 }
